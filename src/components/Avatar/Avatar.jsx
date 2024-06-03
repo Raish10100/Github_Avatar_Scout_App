605ecf762b344035256eb26277a9ad1003a46b87
Raish10100 <raishgma101001@gmail.com>
2024-06-03 21:51:08 +0530

    Updated demo avatar

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import downloadAvatar from '../../utils/downloadAvatar';
 import './Avatar.css'
+import axios from 'axios';
 
 const Avatar = ({username,inputValueLength}) => {
 
@@ -11,7 +12,6 @@ async function fetchData() {
   // console.log(`input length : ${inputValueLength}`)
     try {
       const userAvatarUrl = await downloadAvatar(username);
-      console.log("debugging")
       setAvatarUrl(userAvatarUrl);
       setProfileUrl(`https://github.com/${username}`)
     } catch (error) {
@@ -19,9 +19,50 @@ async function fetchData() {
     }
    
 }
-fetchData()
 
-const demoAvatar = ["Raish10100","Sanket","Santosh","Amitabh","Rahul","Hitesh","Harshil","Riyaz","Aryan"]
+// const demoAvatar = ["Raish10100","Sanket","Santosh","Amitabh","Rahul","Hitesh","Harshil","Riyaz","Aryan"]
+
+const demoAvatar = [
+  {
+    avatarName: 'Raish10100',
+    avatarUrl: "https://avatars.githubusercontent.com/u/137144452?v=4"
+  },
+  {
+    avatarName: 'Sanket',
+    avatarUrl: "https://avatars.githubusercontent.com/u/20416070?v=4"
+  },
+  {
+    avatarName: 'Santosh',
+    avatarUrl: "https://avatars.githubusercontent.com/u/1515991?v=4"
+  },
+  {
+    avatarName: 'Amitabh',
+    avatarUrl: "https://avatars.githubusercontent.com/u/98746?v=4"
+  },
+  {
+    avatarName: 'Rahul',
+    avatarUrl: "https://avatars.githubusercontent.com/u/858220?v=4"
+  },
+  {
+    avatarName: 'Hitesh',
+    avatarUrl: "https://avatars.githubusercontent.com/u/122959?v=4"
+  },
+  {
+    avatarName: 'Harshil',
+    avatarUrl: "https://avatars.githubusercontent.com/u/107907?v=4"
+  },
+  {
+    avatarName: 'Riyaz',
+    avatarUrl: "https://avatars.githubusercontent.com/u/22842?v=4"
+  },
+  {
+    avatarName: 'Aryan',
+    avatarUrl: "https://avatars.githubusercontent.com/u/68467952?v=4"
+  }
+];
+
+
+fetchData()
 
 
 return (
@@ -47,11 +88,11 @@ return (
       <div className="avatar-container">
         {demoAvatar.map((avatar, index) => (
           <div className="avatar" key={index}>
-            <span className="avatar-name">{avatar}</span>
-            <a href={`https://github.com/${avatar}`} target='_blank'  rel="noopener noreferrer">
+            <span className="avatar-name">{avatar.avatarName}</span>
+            <a href={`https://github.com/${avatar.avatarName}`} target='_blank'  rel="noopener noreferrer">
               <img
                 className="avatar-image"
-                src={`https://randomuser.me/api/portraits/men/${index + 1}.jpg`}
+                src={avatar.avatarUrl}
                 alt={`Avatar ${index + 1}`}
               />
             </a>
